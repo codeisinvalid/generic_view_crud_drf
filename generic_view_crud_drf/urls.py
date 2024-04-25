72a092876bcecf753ec38fd436ec6e3a9840adf7
sooj <soojksh@gmail.com>
2024-04-25 19:10:21 +0530

    appended functionalities, embedding the classes

--- generic_view_crud_drf/urls.py
+++ generic_view_crud_drf/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import StudentAPI
+from api.views import RUDStudentAPI, LCStudentAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student-api/', StudentAPI.as_view()),
-    path('student-api/,int:pk>', StudentAPI.as_view()),
+    path('student-api/', LCStudentAPI.as_view()),
+    path('student-api/<int:pk>', RUDStudentAPI.as_view()),
 ]
